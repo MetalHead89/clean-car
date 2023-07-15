526d9f87dc2f8aa965b08460608636c01735adee
Bakalov Vitaliy <bakalov.vitaliy@gmail.com>
2023-07-15 08:52:53 +0700

    Added vitest aliases

--- tests/components/UiNoLocation.test.ts
+++ tests/components/UiNoLocation.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
-import UiNoLocation from '../../components/UiNoLocation.vue'
+import UiNoLocation from '@/components/UiNoLocation.vue'
 
 describe('UiNoLocation component', () => {
   it('UiNoLocation is mounted', () => {
